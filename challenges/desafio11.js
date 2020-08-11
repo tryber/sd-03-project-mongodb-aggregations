3c468e140a366165f24ea434989ba35624f3fcd7
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-11 19:08:21 -0300

    11.js maior

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -9,7 +9,7 @@ db.trips.aggregate([
     _id: "$diaDaSemana",
     total: { $sum: 1 },
   } },
-  { $sort: { total: 1 } },
+  { $sort: { total: -1 } },
   { $limit: 1 },
   { $project: {
     _id: 0,
