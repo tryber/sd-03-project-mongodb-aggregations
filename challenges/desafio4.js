8737b3abec9d58ad8ead30364c130878e7f22b0b
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-11 16:31:33 -0300

    4.js

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,11 +1,11 @@
 db.movies.aggregate([
   { $project: {
     _id: 0,
-    title_split: { $split: "$title" }
+    title_split: { $split: ["$title", " "] }
   } },
   { $match: {
-    title_split: [{ $size: 1 }, " "]
+    title_split: { $size: 1 }
   } },
-  { $unwind: title_split },
-  { $sort: { title_split: 1} },
+  { $unwind: "$title_split" },
+  { $sort: { title_split: 1 } },
 ]);
