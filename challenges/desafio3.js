0754730138406a6901269f95db042f1eac9771c5
potew <potew0@gmail.com>
2020-08-11 18:15:19 -0300

    1 ao 6

---
+++ challenges/desafio3.js
@@ -0,0 +1,28 @@
+//  Agora que você tem os campos essenciais, retorne esses filmes ordenados por ano e nota IMDB de forma decrescente e por ordem alfabética.
+
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year"
+    }
+  },
+  {
+    $sort: {
+      notaIMDB: -1,
+      titulo: 1
+    }
+  }
+]).pretty();
