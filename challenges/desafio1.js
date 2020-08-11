ac094e471d207e773f1f9be15f91532573e3dec1
Marco Barbosa <marco.meireles.b@gmail.com>
2020-08-11 18:06:30 -0300

    desafio1

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,7 @@
 // commit inicial
+db.movies.aggregate([
+  {$match: {"imdb.rating": {$gte: 7}}},
+  {$match: {$nor: [{"genres": "Crime"}, {"genres": "Horror"}]}},
+  {$match: {$or: [{"rated": "PG"}, {"rated": "G"}]}},
+  {$match: {$and: [{"languages": "English"}, {"languages": "Spanish"}]}}
+]).pretty();
