df87323fad64c71718dd463b8d4896238f125fec
luldelrey <rpgstrikered@gmail.com>
2020-08-11 14:34:30 -0300

    more challenges

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,7 +2,7 @@ db.movies.aggregate([{
   $match: {
     "imdb.rating": { $gte: 7},
     "genres": { $nin: ["Crime", "Horror"]},
-    "languages": { $in: ["English", "Spanish"]},
+    "languages": { $all: ["English", "Spanish"]},
     $or: [
       { "rated": "PG" },
       { "rated": "G" }
