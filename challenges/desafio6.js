fbbaa51065935602162dbc03d0323fd2cd2631cc
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-11 15:59:53 -0300

    formating 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -9,8 +9,11 @@ db.movies.aggregate([
     media_rating : { $avg: "$imdb.rating" },
     desvio_padrao : { $stdDevSamp: "$imdb.rating" }
   } },
-  { $addFields: {
+  { $project: {
+    _id: 0,
+    maior_rating: 1,
+    menor_rating: 1,
     media_rating : { $round: ["$media_rating", 1] },
     desvio_padrao : { $round: ["$desvio_padrao", 1] }
-  }}
+  } }
 ]);
