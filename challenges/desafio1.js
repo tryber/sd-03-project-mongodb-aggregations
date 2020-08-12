f6821f02340c46de5ec9ad1b8bc7a3fc5a5d0694
Lucas Franca <luc.rangel90@gmail.com>
2020-08-12 16:30:24 -0300

    desafio 1 corrigido

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,32 +1,12 @@
 db.movies.aggregate([
   {
     $match: {
-      "imdb.rating": {
-        $gte: 7,
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: {
+        $all: ["English", "Spanish"],
       },
-      genres: {
-        $nin: ["Crime", "Horror"],
-      },
-    },
-  },
-  {
-    $match: {
-      $or: [
-        {
-          rated: "PG",
-          rated: "G",
-        },
-      ],
-    },
-  },
-  {
-    $match: {
-      $and: [
-        {
-          languages: "English",
-          languages: "Spanish",
-        },
-      ],
     },
   },
 ]);
