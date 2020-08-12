398c52e9374d4f311cd44974c8ed39c141b4783f
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-11 22:10:19 -0300

    11.js

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -7,13 +7,13 @@ db.trips.aggregate([
   } },
   { $group: {
     _id: "$diaDaSemana",
-    total: { $sum: 1 },
+    qntDeViagens: { $sum: 1 },
   } },
   { $sort: { total: -1 } },
   { $limit: 1 },
   { $project: {
     _id: 0,
-    total: 1,
     diaDaSemana: "$_id",
+    total: "$qntDeViagens",
   } },
 ]);
