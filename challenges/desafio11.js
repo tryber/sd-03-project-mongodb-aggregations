3813f32e0aa4b3b40142365ba493b1f322177620
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-11 22:12:13 -0300

    11 correcao de nome total

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,19 +1,19 @@
 db.trips.aggregate([
   { $match: {
-    startTime: { $exists: 1 }
+    "startTime": { $exists: 1 }
   } },
   { $addFields: {
-    diaDaSemana: { $dayOfWeek: "$startTime" },
+    "diaDaSemana": { $dayOfWeek: "$startTime" },
   } },
   { $group: {
-    _id: "$diaDaSemana",
-    qntDeViagens: { $sum: 1 },
+    "_id": "$diaDaSemana",
+    "qntDeViagens": { $sum: 1 },
   } },
-  { $sort: { total: -1 } },
+  { $sort: { "qntDeViagens": -1 } },
   { $limit: 1 },
   { $project: {
-    _id: 0,
-    diaDaSemana: "$_id",
-    total: "$qntDeViagens",
+    "_id": 0,
+    "diaDaSemana": "$_id",
+    "total": "$qntDeViagens",
   } },
 ]);
