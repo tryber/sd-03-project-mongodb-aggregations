ea35ce904f22df1d815cd706595cca3b4bd490c0
DiegoRafaelAF <draf0314@gmail.com>
2020-08-11 22:28:15 -0300

    finish project

---
+++ challenges/desafio10.js
@@ -0,0 +1,31 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      tempo: {
+        $abs: {
+          $subtract: ["startTime", "stopTime"],
+        },
+      },
+    },
+  },
+  {
+    $group: {
+      _id: $usertype,
+      duracaoMedia: { $avg: "tempo" },
+    },
+  },
+  {
+    $projject: {
+      _id: 1,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: [
+          {
+            $divide: [{ $avg: "duracaoMedia" }, 3600000],
+          },
+          2,
+        ],
+      },
+    },
+  },
+]);
---
+++ challenges/desafio11.js
@@ -0,0 +1,27 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: { $exists: 1 },
+    },
+  },
+  {
+    $addFields: {
+      diaDaSemana: { $dayOfWeek: "$startTime" },
+    },
+  },
+  {
+    $group: {
+      _id: "diaDaSemana",
+      total: { $sum: 1 },
+    },
+  },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  {
+    $project: {
+      _id: 0,
+      diaDaSemana: "$_id",
+      total: 1,
+    },
+  },
+]);
