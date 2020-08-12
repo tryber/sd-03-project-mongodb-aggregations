0bb9748e864bd66e0a31c97ef7ffa5c190cf30a2
William Igor <williamigorsilva@gmail.com>
2020-08-12 12:39:51 -0300

    o 7 tá me trolando

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -5,14 +5,14 @@ db.movies.aggregate([
     $group: {
       "_id": "$cast",
       "numeroFilmes": { $sum: 1 },
-      "media_rating": { $avg: "$imdb.rating" }
+      "mediaIMDB": { $avg: "$imdb.rating" }
     }
   },
   {
     $project: {
       "_id": 1,
       "numeroFilmes": 1,
-      "media_rating": { $round: ["$media_rating",1] }
+      "mediaIMDB": { $round: ["$mediaIMDB",1] }
     }
   },
   { $sort: { "numeroFilmes": -1, "_id": -1 } }
