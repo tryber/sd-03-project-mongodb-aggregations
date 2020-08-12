9fa5b4a7d29a2f732f939941bfadf547bc11241a
tales <talestales@gmail.com>
2020-08-12 08:08:26 -0300

    a

---
+++ challenges/desafio10.js
@@ -0,0 +1,25 @@
+const convertToHour = 1000 * 60 * 60;
+
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$usertype",
+      duracaoMedia: {
+        $avg: {
+          $subtract: ["$stopTime", "$startTime"]
+        }
+      },
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: {
+        $round: [
+          { $divide: ["$duracaoMedia", convertToHour] }, 2
+        ]
+      }
+    }
+  }
+]);
