2fc1f9a4a8e1d86d4549b80d00acde964561c868
Lucas Werner <lucas.1werner@gmail.com>
2020-08-12 16:59:18 -0300

    CC

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -17,11 +17,11 @@ db.movies.aggregate([
   },
   {
     $project: {
-      "_id": 0,
-      "maior_rating": { $round: ["$maior_rating", 1]},
-      "menor_rating": { $round: ["$menor_rating", 1]},
-      "media_rating": { $round: ["$media_rating", 1]},
-      "desvio_padrao": { $round: ["$desvio_padrao", 1]}
-    }
-  }
+      _id: 0,
+      maior_rating: { $round: ["$maior_rating", 1] },
+      menor_rating: { $round: ["$menor_rating", 1] },
+      media_rating: { $round: ["$media_rating", 1] },
+      desvio_padrao: { $round: ["$desvio_padrao", 1] },
+    },
+  },
 ]);
