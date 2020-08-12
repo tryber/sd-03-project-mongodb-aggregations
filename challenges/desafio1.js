1546f3265123704b8c0bcd4e9fe6a809f8092f16
rsfdev <roger_faustino@hotmail.com>
2020-08-12 11:18:35 -0300

    Aggregations Project

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,5 @@
-starting
+db.movies.aggregate([
+  { $match: { "imdb.rating": { $gte: 7 }, genres: { $nin: ["Horror", "Crime"] }, rated: { $in: ["PG", "G"] }, languages: { $all: ["English", "Spanish"] }
+  }
+  }
+]);
