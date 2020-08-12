b6486354730b3f545669821cee83a9b6fff6af1d
rsfdev <roger_faustino@hotmail.com>
2020-08-12 12:30:01 -0300

    Aggregate Project

---
+++ challenges/desafio8.js
@@ -0,0 +1,21 @@
+db.air_routes.aggregate([
+  {
+    $match : {
+      $or: [{"airplane": "747"}, {"airplane": "380"}]
+    }
+  },
+  {
+    $lookup: {
+      from: "air_alliances",
+      let: { airline_name: "$airline.name" },
+      pipeline: [
+        {$unwind: "$airlines"},
+        { $match: {$expr: {$eq: [ "$airlines", "$$airline_name" ]}}},
+        { $project: {  _id: 0, name: 1 }}
+      ], as: "routes"
+    }
+  },
+  { $group: { _id: "$routes.name", totalRotas: {$sum : 1}}},
+  { $unwind: "$_id" },
+  {$sort: {"totalRotas":-1}}, {$limit:1}
+]).pretty();
---
+++ challenges/desafio9.js
@@ -0,0 +1,10 @@
+db.trips.aggregate([{ $match: { birthYear: {$nin: [""]}}},
+{
+  $group:{
+    _id: 0,
+    maiorAnoNascimento:{ $max: {$toInt:"$birthYear"}},
+    menorAnoNascimento: { $min: {$toInt:"$birthYear"}},
+  }
+},
+  { $project: { _id: 0, maiorAnoNascimento: 1, menorAnoNascimento: 1}}
+]);
