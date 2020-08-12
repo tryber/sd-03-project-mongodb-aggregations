fe2cca7b45c69b303b4181631792cd7f0de188ef
fabemiliano <fab.emiliano@gmail.com>
2020-08-11 22:49:12 -0300

    all good

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -2,6 +2,6 @@ db.trips.aggregate([
   { $addFields: { tripTime: { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60 * 1000] } } },
   { $group: { _id: "$bikeid", media: { $avg: "$tripTime" } } },
   { $sort: { media: -1 } },
-  { $project: { _id: 0, bikeId: "$bikeid", duracaoMedia: { $ceil: "$media" } } },
+  { $project: { _id: 0, bikeId: "$_id", duracaoMedia: { $ceil: "$media" } } },
   { $limit: 5 }
 ]);
--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -5,13 +5,17 @@ db.air_alliances.aggregate([
       from: "air_routes",
       let: { al: "$airlines" },
       pipeline: [
-        { $match: {$and: [{$expr: {$eq: ["$airline.name", "$$al"]}},{ airplane: { $in: ["747", "380"] }  }] }},
+        { $match: { $and: [{ $expr: { $eq: ["$airline.name", "$$al"] } }, { airplane: { $in: ["747", "380"] } }] } },
         { $group: { _id: "$airline.name", count: { $sum: 1 } } },
-        { $project:  {_id: 1, count: 1} }
+        { $project: { _id: 1, count: 1 } }
       ],
       as: "data"
     }
   },
-  {$unwind: "$data"},
-  {$project: {_id: "$data._id", totalRotas: "$data.count"}}
+  { $unwind: "$data" },
+  { $project: { _id: "$data._id", name: "$name", totalRotas: "$data.count" } },
+  { $group: { _id: "$name", total: { $sum: "$totalRotas" } } },
+  { $sort: { total: -1 } },
+  { $limit: 1 },
+  { $project: { _id: "$_id", totalRotas: "$total" } }
 ]);
