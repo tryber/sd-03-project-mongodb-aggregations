331d9c3d0c9bb2d4a7c99443480872b223124648
Gustaff <gustavohmfigueiredo@gmail.com>
2020-08-11 21:45:38 -0300

    12.js

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,9 +1,9 @@
 db.trips.aggregate([
   { $group: {
-    _id: { $dayOfWeek: "$startTime" },
-    total: { $sum: 1 },
+    "_id": { $dayOfWeek: "$startTime" },
+    "total": { $sum: 1 },
   } },
-  { $sort: { total: -1 } },
+  { $sort: { "total": -1 } },
   { $limit: 1 },
   { $lookup: {
     from: "trips",
@@ -15,22 +15,22 @@ db.trips.aggregate([
         }
       } },
       { $project: {
-        _id: 0,
-        name: "$startStationName"
+        "_id": 0,
+        "name": "$startStationName"
       } },
     ],
     as: "stations"
   } },
   { $unwind: "$stations" },
   { $group: {
-    _id: "$stations.name",
-    total: { $sum: 1 }
+    "_id": "$stations.name",
+    "total": { $sum: 1 }
   } },
   { $project: {
-    _id: 0,
+    "_id": 0,
     "nomeEstacao" : "$_id",
-    "total" : 1
+    "total" : 1,
   } },
-  { $sort: { total: -1 } },
+  { $sort: { "total": -1 } },
   { $limit: 1 }
 ]);
