18158e91e436b131bd60961e5102eca301ed21b4
rsfdev <roger_faustino@hotmail.com>
2020-08-13 18:31:13 -0300

    Aggregations Project

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,5 +1,5 @@
 db.trips.aggregate([{ $addFields: { "duracao": { $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60 * 1000 ]}}},
-  { $group: { "_id": "$bikeid", "duracaoMediaMinutos": { $avg: "$duracaoEmMinutos" }}},
-  { $project: { "_id": 0, "bikeId":"$_id" , "duracaoMedia": { $ceil: "$duracaoMediaMinutos" }}},
+  { $group: { "_id": "$bikeid", "duracaoMediaMin": { $avg: "$duracao" }}},
+  { $project: { "_id": 0, "bikeId":"$_id" , "duracaoMedia": { $ceil: "$duracaoMediaMin" }}},
   {$sort: { "duracaoMedia": -1 } }, { $limit: 5 }
 ]);
