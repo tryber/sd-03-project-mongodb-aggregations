495e6c713b351de8efd994363657f14dfd7a5ad9
nato-x <alvares.renato@hotmail.com>
2020-08-13 14:54:11 -0300

    CC fixos

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,22 +1,28 @@
 db.trips.aggregate([
-    {
-        $addFields: {
-          duracaoEmMinutos: {
-            $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000],
-          },
-        },
+  {
+    $addFields: {
+      duracaoEmMinutos: {
+        $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000],
       },
-    {$group:{
-        _id: "$bikeid",
-        duracaoMedia:{ $avg: "$duracaoEmMinutos" } 
-    }},
-    {$project:{
-        _id: 0, 
-        bikeId:"$_id",
-        duracaoMedia: {$ceil: "$duracaoMedia"}
-    }},
-    {$sort:{
-        duracaoMedia: -1
-    }},
-    {$limit: 5}
-])
+    },
+  },
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: { $avg: "$duracaoEmMinutos" },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: "$duracaoMedia" },
+    },
+  },
+  {
+    $sort: {
+      duracaoMedia: -1,
+    },
+  },
+  { $limit: 5 },
+]);
