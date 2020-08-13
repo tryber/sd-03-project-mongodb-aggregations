ee2dc94bdc08c685a193527833307c26acad7cc1
Marco Barbosa <marco.meireles.b@gmail.com>
2020-08-13 10:34:44 -0300

    refatorando exercicio 3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -8,7 +8,7 @@ db.movies.aggregate([
     }
   },
   {$project: {"_id": 0,titulo: "$title", avaliado: "$rated", notaIMDB: "$imdb.rating", votosIMDB: "imdb.votes", "ano":"$year"}},
-  {$sort: {ano: -1, notaIMDB: 1}}
+  {$sort: {ano: -1, notaIMDB: -1, titulo: 1}}
 ]).pretty();
 
 // db.movies.aggregate([
@@ -21,5 +21,5 @@ db.movies.aggregate([
 //     }
 //   },
 //   {$project: {"_id": 0,"titulo": "$title", "avaliado": "$rated", "notaIMDB": "$imdb.rating", "votosIMDB": "imdb.votes", "ano":"$year"}},
-//   {$sort: {"ano": -1, notaIMDB: 1}}
+//   {$sort: {"ano": -1, "notaIMDB": -1, "titulo": 1}}
 // ]).pretty();
