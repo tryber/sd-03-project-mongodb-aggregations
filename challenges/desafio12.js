babf84fd86dc75de52e2e228703057e41b5a36a7
nato-x <alvares.renato@hotmail.com>
2020-08-13 21:41:44 -0300

    d12, c/ linha no fim

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,33 +1,33 @@
 db.trips.aggregate([
-    {
-      $addFields: {
-        diaDaSemana: {
-          $dayOfWeek: "$startTime",
-        },
+  {
+    $addFields: {
+      diaDaSemana: {
+        $dayOfWeek: "$startTime",
       },
     },
-    {
-        $match:{
-            diaDaSemana: 5
-    }},
-    {
-      $group: {
-        _id: "$startStationName",
-        total: { $sum: 1 },
-      },
+  },
+  {
+    $match: {
+      diaDaSemana: 5,
     },
-    {
-      $project: {
-          startStationName: "$_id",
-          total: 1,
-          _id: 0,
-      },
+  },
+  {
+    $group: {
+      _id: "$startStationName",
+      total: { $sum: 1 },
     },
-    {
-      $sort: {
-        total: -1,
-      },
+  },
+  {
+    $project: {
+      startStationName: "$_id",
+      total: 1,
+      _id: 0,
     },
-    { $limit: 1 },
-  ]);
-  
+  },
+  {
+    $sort: {
+      total: -1,
+    },
+  },
+  { $limit: 1 },
+]);
