db243cc51504e6447ea2ea3fd9ad7eb3e687e993
nato-x <alvares.renato@hotmail.com>
2020-08-13 21:40:52 -0300

    d12, tá invertido, mas objeto

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,20 +1,33 @@
 db.trips.aggregate([
-    {$addFields:{
-        nomeEstacao:{
-            $dayOfWeek:"$startTime"
-        }
+    {
+      $addFields: {
+        diaDaSemana: {
+          $dayOfWeek: "$startTime",
+        },
+      },
+    },
+    {
+        $match:{
+            diaDaSemana: 5
     }},
-    {$group:{
-        _id:"$nomeEstacao",
-        total:{$sum:1}
-    }},
-    {$sort:{
-        total: -1
-    }},
-    {$limit : 1},
-    {$project:{
-        nomeEstacao:"$_id",
-        _id: 0,
-        total: 1
-    }}
-])
+    {
+      $group: {
+        _id: "$startStationName",
+        total: { $sum: 1 },
+      },
+    },
+    {
+      $project: {
+          startStationName: "$_id",
+          total: 1,
+          _id: 0,
+      },
+    },
+    {
+      $sort: {
+        total: -1,
+      },
+    },
+    { $limit: 1 },
+  ]);
+  
