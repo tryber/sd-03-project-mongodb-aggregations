5b403ae0e584c0f00d4d38be90a8492d90c850cf
mirellaalves <mirella.alves@gmail.com>
2020-08-16 19:54:19 -0300

    desafio2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -3,3 +3,19 @@
 // O resultado da sua query deve ter o seguinte formato:
 // { "titulo" : "A Streetcar Named Desire", "avaliado" : "PG", "notaIMDB" : 8.1, "votosIMDB" : 72364, "ano" : 1951 }
 // // Demais documentos
+db.movies.aggregate([
+  { $match: {
+    "imdb.rating": { $gte: 7 },
+    genres: { $nin: [ "Crime", "Horror" ] },
+    rated: { $in: [ "PG", "G" ] },
+    languages: { $all: [ "English", "Spanish" ]},
+  }},
+  { $project: {
+    _id: 0,
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year"
+  }}
+]);
