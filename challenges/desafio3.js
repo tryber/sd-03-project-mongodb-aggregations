3f3d37d101405b42b2e88ed70e806576a4f13ca9
mirellaalves <mirella.alves@gmail.com>
2020-08-16 20:00:16 -0300

    desafio3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -3,3 +3,20 @@
 // O resultado da sua query deve ter o seguinte formato:
 // { "titulo" : "McFarland, USA", "avaliado" : "PG", "notaIMDB" : 7.5, "votosIMDB" : 14091, "ano" : 2015 }
 // // Demais documentos
+db.movies.aggregate([
+  { $match: {
+    "imdb.rating": { $gte: 7 },
+    genres: { $nin: [ "Crime", "Horror" ] },
+    rated: { $in: [ "PG", "G" ] },
+    languages: { $all: [ "English", "Spanish" ] },
+  } },
+  { $project: {
+    _id: 0,
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imbd.votes",
+    ano: "$year"
+  } },
+  { $sort: { ano: -1, notaIMDB: -1, titulo: 1 } }
+]);
