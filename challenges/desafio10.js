08a7ea967767eed2df85f8eb967486d301c99c60
mirellaalves <mirella.alves@gmail.com>
2020-08-17 18:38:18 -0300

    desafio10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -3,3 +3,15 @@
 // O resultado da sua query deve ter o seguinte formato:
 // { "tipo" : <tipo>, "duracaoMedia" : <duracaoMedia> }
 // // ...
+db.trips.aggregate([
+  { $group: {
+    _id: "$usertype",
+    duracaoMedia: { $avg: { $subtract: [ "$stopTime", "$startTime" ] } }
+  } },
+  { $project: {
+    _id: 0,
+    tipo: "$_id",
+    duracaoMedia: { $round: [ { $divide: [ "$duracaoMedia", 1000*60*60 ] }, 2 ] }
+  } },
+  { $sort: { duracaoMedia: 1 } }
+]);
