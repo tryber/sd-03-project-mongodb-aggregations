8c1c5c32721a4a2bac81b4310ec35095fac11e0b
mirellaalves <mirella.alves@gmail.com>
2020-08-17 00:50:11 -0300

    desafio6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -4,7 +4,6 @@
 // Dica: todos os filmes na coleção, que já ganharam um Oscar, começam com uma sequência de string parecida com essas abaixo, portanto $regex é um operador bem-vindo:
 // Won 10 Oscars
 // Won 1 Oscar
-
 // Utilizem o $stdDevSamp para calcular o desvio padrão.
 // O resultado da sua query deve ter o seguinte formato:
 // {
@@ -13,3 +12,24 @@
 //   "media_rating" : <media_rating>,
 //   "desvio_padrao" : <desvio_padrao>
 // }
+db.movies.aggregate([
+  { $match: { $and: [
+    {awards: { $regex: /^Won/i }},
+    {awards: { $regex: /oscar/i }}
+  ]}},
+  { $sort: { "imdb.rating": 1 }},
+  { $group: {
+    _id: null,
+    maior_rating: { $last: "$imdb.rating" },
+    menor_rating: { $first: "$imdb.rating" },
+    media_rating: { $avg: "$imdb.rating" },
+    desvio_padrao: { $stdDevSamp: "$imdb.rating" }
+  }},
+  { $project: {
+    _id: 0,
+    maior_rating: 1,
+    menor_rating: 1,
+    media_rating: { $round: [ "$media_rating", 1 ] },
+    desvio_padrao: { $round: [ "$desvio_padrao", 1 ] }
+  }}
+]);
