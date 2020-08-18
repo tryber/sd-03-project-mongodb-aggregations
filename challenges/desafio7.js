40e19bf8d6eb0e213a13dc203d9e849d03d9e8cd
DevT <aguiarthiag@gmail.com>
2020-08-18 20:23:49 -0300

    Correction challenge 7

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -1,23 +1,13 @@
 db.movies.aggregate([
-  {
-    $match: {
-      languages: { $in: [ "English" ] }
-    }
-  },
-  {
-    $unwind: "$cast"
-  },
+  { $match: { languages: { $in: [ "English" ] } } },
+  { $unwind: "$cast" },
   { 
     $group: {
       _id: "$cast",
       numeroFilmes: { $sum: 1 },
-      mediaIMDB: { $round: [ { $avg: "$imdb.rating" }, 1 ] }
+      mediaIMDB: { $avg: "$imdb.rating" } 
     }
   }, 
-  {
-    $sort: {
-      numeroFilmes: -1,
-      _id: -1
-    }
-  }
+  { $sort: { numeroFilmes: -1, _id: -1 } },
+  { $project: { numeroFilmes: 1, mediaIMDB: { $round: ["$mediaIMDB", 1] } } }
 ]);
