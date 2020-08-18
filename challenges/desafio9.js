f8b6adb207b9a5f07b22f17acf2d9417506fcfea
Lucas Franca <luc.rangel90@gmail.com>
2020-08-18 18:04:21 -0300

    desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,24 @@
+db.trips.aggregate([
+  {
+    $match: {
+      birthYear: { $exists: 1, $ne: "" },
+    },
+  },
+  {
+    $project: {
+      birthYear: {
+        $toInt: "$birthYear",
+      },
+    },
+  },
+  {
+    $group: {
+      _id: 1,
+      maiorAnoNascimento: { $max: "$birthYear" },
+      menorAnoNascimento: { $min: "$birthYear" },
+    },
+  },
+  {
+    $project: { _id: 0 },
+  },
+]);
