5d7eae29e67908a2fd9dc1c7048d481c9985044f
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-08-19 00:35:55 -0300

    Desafio 2 finalizado

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,7 +2,7 @@ db.movies.aggregate([
   {
     $match: {
       $and: [
-        { "imdb.rating": { $gt: 7} },
+        { "imdb.rating": { $gte: 7} },
         { genres: { $nin: ["Crime", "Horror"]}},
         { rated: { $in: ["PG", "G"]}},
         { languages: { $all: ["English", "Spanish"]}}
