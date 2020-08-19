bfef621f977fbdea959193cfcac7458595afdda3
Gabriel <dreadpool38@gmail.com>
2020-08-19 12:25:52 -0300

    corrigido 13 e 14

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -2,6 +2,6 @@ db.trips.aggregate([
   {$addFields:{date:{$dateToString:{date:"$startTime",format:"%d/%m/%Y"}},duration:{$divide:[{$subtract:["$stopTime","$startTime"]},60000]}}},    
   {$match:{date:"10/03/2016"}},
   {$group:{_id:null,"duracaoMediaEmMinutos":{$avg:"$duration"}}},
-  {$project:{"duracaoMediaEmMinutos":{$ceil:"$duracaoMediaEmMinutos"}}}
+  {$project:{_id:0,"duracaoMediaEmMinutos":{$ceil:"$duracaoMediaEmMinutos"}}}
 
 ])
--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,8 +1,8 @@
-// comment
 db.trips.aggregate([
-  {$addFields:{date:{$dateToString:{date:"$startTime",format:"%d/%m/%Y"}},duration:{$divide:[{$subtract:["$stopTime","$startTime"]},60000]}}},    
+  {$addFields:{duration:{$divide:[{$subtract:["$stopTime","$startTime"]},60000]}}},
+  {$group:{_id:"$bikeid",duration:{$avg:"$duration"}}}, 
   {$sort:{duration:-1}},
-  {$project:{_id:0,bikeid:1,"duracaoMedia":{$ceil:"$duration"}}},
+  {$project:{_id:0,bikeid:"$_id","duracaoMedia":{$ceil:"$duration"}}},
   {$limit:5}
 
-],{allowDiskUse: true})
+])
