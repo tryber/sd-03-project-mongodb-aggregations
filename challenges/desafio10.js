ecf3adc3ac94cc5c425af1cce47a69e5ca560769
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-08-20 02:59:20 -0300

    Desafio 10 finalizado

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -2,7 +2,18 @@ db.trips.aggregate([
   {
     $group: {
       _id: "$usertype",
-      duracaoMedia: { $avg: { $subtract: ["$stopTime", "$startTime"] }}
+      duracaoMedia: {
+        $avg: {
+          $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 1000 * 60 * 60]
+        }
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      tipo: "$_id",
+      duracaoMedia: { $round: ["$duracaoMedia", 2]}
     }
   }
 ])
