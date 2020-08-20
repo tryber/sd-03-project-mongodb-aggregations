ad51f148efd2a7bba7620a8ee40814cfd551458f
Gabriel <dreadpool38@gmail.com>
2020-08-20 13:19:26 -0300

    formatting

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,8 +1,15 @@
 db.trips.aggregate([
-  {$addFields:{duration:{$divide:[{$subtract:["$stopTime","$startTime"]},60000]}}},
-  {$group:{_id:"$bikeid",duration:{$avg:"$duration"}}}, 
-  {$sort:{duration:-1}},
-  {$project:{_id:0,bikeid:"$_id","duracaoMedia":{$ceil:"$duration"}}},
-  {$limit:5}
-
+  {
+    $addFields: {
+      duration: {
+        $divide: [{ $subtract: ["$stopTime", "$startTime"] }, 60000],
+      },
+    },
+  },
+  { $group: { _id: "$bikeid", duration: { $avg: "$duration" } } },
+  { $sort: { duration: -1 } },
+  {
+    $project: { _id: 0, bikeid: "$_id", duracaoMedia: { $ceil: "$duration" } },
+  },
+  { $limit: 5 },
 ]);
