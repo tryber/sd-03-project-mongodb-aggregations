e0333330417ee23ecec91217b6cc7e2ef4606649
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-08-21 19:11:11 -0300

    desafio7

---
+++ challenges/desafio7.js
@@ -0,0 +1,25 @@
+db.movies.aggregate([
+  {
+    $match: {languages: {$all: ["English"]}}
+  },
+  {$unwind: "$cast"},
+  {
+    $group: {
+      "_id": "$cast",
+      "countFilmes": {$sum: 1},
+      "avgIMDB": {$avg: "$imdb.rating" }
+    }
+  },
+  {
+    $project: {
+      "numeroFilmes": "$countFilmes",
+      "mediaIMDB": {$round: ["$avgIMDB", 1]}
+    }
+  },
+  {
+    $sort: {
+      "numeroFilmes": -1,
+      "_id": -1
+    }
+  }
+]);
