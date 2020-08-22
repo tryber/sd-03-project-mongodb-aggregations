c42fa2662a7405763fea17a27e5259f5a973e4e6
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-08-21 22:30:10 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,34 @@
+db.movies.aggregate([
+    {
+        $match: {
+            "countries": "USA",
+            "tomatoes.viewer.ratings": {$gte: 3}
+        }
+    },
+    {
+        $addFields: {
+            "lister": ["Sandra Bullock","Tom Hanks","Julia Roberts","Kevin Spacey","George Clooney"],
+            "favs": {$setIntersection: ["$lister","$cast"]},
+            "num_favs": {$size: "$favs"}
+        }
+    },
+    {
+        $sort: {
+            "num_favs": -1,
+            "tomatoes.viewer.rating": -1,
+            "title": -1
+        }
+    },
+    {
+        $projection: {
+            "title": 1,
+            "_id": 0
+        }
+    },
+    {
+        $skip: 24
+    },
+    {
+        $limit: 1
+    }
+]);
