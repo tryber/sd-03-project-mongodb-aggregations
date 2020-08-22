3178f1b9fa53abc102d9640fc2ca1f002ed784d7
Gabriel Oliveira Ferreira <gabril1335555@gmail.com>
2020-08-21 23:45:04 -0300

    desfio14 fixed

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -4,19 +4,19 @@ db.trips.aggregate([
       "duracaoMedia": {$avg: { $subtract: [ "$stopTime", "$startTime" ]}}
     }},
   {$addFields: {
-      "duration": {
-        $divide: [ "$duration", 60 * 1000 ]
+      "duracaoMedia": {
+        $divide: [ "$duracaoMedia", 60 * 1000 ]
       }
     }},
   {$sort: {
-      "duration": -1
+      "duracaoMedia": -1
     }},
   {$limit: 5},
   {$project: {
       "_id": 0,
       "bikeId": "$_id",
-      "duration": {
-        $ceil: "$duration"
+      "duracaoMedia": {
+        $ceil: "$duracaoMedia"
       }
     }}
 ]);
