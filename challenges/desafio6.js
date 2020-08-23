f41a4fa21e9d08d3724e35270fd1e2580b1d5ece
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-08-23 14:22:39 -0300

    esqueci o round

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -8,8 +8,8 @@ db.movies.aggregate([
         $project: {
             "maior_rating": {$max: "$imdb.rating"},
             "menor_rating": {$min: "imdb.rating"},
-            "media_rating": {$avg: "imdb.rating"},
-            "desvio_padrao": {$stdDevSamp: "imdb.rating"},
+            "media_rating": {$round: [{$avg: "imdb.rating"},1]},
+            "desvio_padrao": {$round: [{$stdDevSamp: "imdb.rating"},1]},
             "_id":0
         }
     }
