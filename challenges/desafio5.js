b8027a0c73fed28be1b0816c37d24749073a6bac
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-08-23 12:53:17 -0300

    existe?

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -2,7 +2,8 @@ db.movies.aggregate([
     {
         $match: {
             "countries": "USA",
-            "tomatoes.viewer.ratings": {$gte: 3}
+            "tomatoes.viewer.ratings": {$gte: 3},
+            "cast": {$exists: 1}
         }
     },
     {
