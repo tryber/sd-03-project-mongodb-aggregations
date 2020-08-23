a209caceff2f12749777130ec37202ee8182826b
Gabryel <gabryel.ryba@gmail.com>
2020-08-23 17:26:21 -0300

    desafio12.js

---
+++ challenges/desafio12.js
@@ -0,0 +1,7 @@
+db.trips.aggregate([
+  { $addFields: { startDate: { $dayOfWeek: "$startTime" } } },
+  { $match: { startDate: { $eq: 5 } } },
+  { $group: { _id: "$startStationName", total: { $sum: 1 } } },
+  { $project: { _id: 0, nomeEstacao: "$_id", total: "$total" } },
+  { $sort: { total: -1 } }, { $limit: 1 }
+]);
