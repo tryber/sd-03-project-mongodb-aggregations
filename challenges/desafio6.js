51840bd027e574d589d8d4e3456f6d9de316d4f0
renandreolle@hotmail.com <renandreolle@hotmail.com>
2020-08-23 22:04:29 -0300

    acho que tinha 2 espaços no regex

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,25 +1,25 @@
 db.movies.aggregate([
     {
         $match: {
-            "awards": {$regex: /Won  \d+ Oscar/i}
+            "awards": {$regex: /Won \d+ Oscar/i}
         }
     },
     {
         $group: {
             "_id": null,
-            "gmaior_rating": {$max: "$imdb.rating"},
-            "gmenor_rating": {$min: "$imdb.rating"},
-            "gmedia_rating": {$avg: "$imdb.rating"},
-            "gdesvio_padrao": {$stdDevSamp: "$imdb.rating"},
+            "maior_rating": {$max: "$imdb.rating"},
+            "menor_rating": {$min: "$imdb.rating"},
+            "media_rating": {$avg: "$imdb.rating"},
+            "desvio_padrao": {$stdDevSamp: "$imdb.rating"},
         }
     },
     {
         $project: {
             "_id":0,
-            "maior_rating": "$gmaior_rating",
-            "menor_rating": "$gmenor_rating",
-            "media_rating": {$round: ["$gmedia_rating",1]},
-            "desvio_padrao": {$round: ["$gdesvio_padrao",1]}
+            "maior_rating": 1,
+            "menor_rating": 1,
+            "media_rating": {$round: ["$media_rating",1]},
+            "desvio_padrao": {$round: ["$desvio_padrao",1]}
         }
     }
 ]);
