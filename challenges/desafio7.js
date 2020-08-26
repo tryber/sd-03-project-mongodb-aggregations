3086ef59e02deff9c1f3370366cb9bd1fdedb28a
wenderson <contato@wendersonbelko.com>
2020-08-26 17:29:39 -0300

    7 8

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -3,23 +3,26 @@ db.movies.aggregate([
         $match:  // Considere somente os membros do elenco de filmes com o idioma inglês (English).
             { "languages": "English" }
     },
-    { $unwind: "$cast" }, // não vai produzir um documento se o valor for nulo
+    {
+        $unwind: "$cast" // não vai produzir um documento se o valor for nulo
+    },
     {
         $group: {  // Queremos contar quantos filmes cada um dos atores e atrizes do elenco (cast) já participou e pega uma media com avg.
             "_id": "$cast",
-            "contarFilmes": { $sum: 1 },
-            "avgIMDB": { $avg: "$imdb.rating" }
+            "mediaIMDB": { $avg: "$imdb.rating" },
+            "numeroFilmes": { $sum: 1 }
         }
     },
     {
         $project: {  // traga o nome do ator ou atriz, número de filmes em que participou e a média do imdb desses filmes arredondada para uma casa decimal usando o operador $round
+            "_id": 1,
             "mediaIMDB": { $round: ["$avgIMDB", 1] },
-            "filmes": 1
+            "numeroFilmes": 1
         }
     },
     {
         $sort: {  // Exiba a lista em ordem decrescente de documentos pelo número de filmes e nome do ator ou atriz.
-            "filmes": -1,
+            "numeroFilmes": -1,
             "_id": -1
         }
     }
--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -4,17 +4,16 @@ db.air_alliances.aggregate([
         $lookup: {
             from: "air_routes", let: { airlines: "$airlines" },
             pipeline: [ // Liste todas as parcerias da coleção air_alliances, que voam rotas com um Boing 747 ou um Airbus A380
-                { $match: { $expr: { $eq: ["$airline.name", "$$airlines"] } } },
-                { $match: { airplane: { $in: ["747", "380"] } } }
+                { $match: { airplane: { $in: ["747", "380"] } },
+                            $expr: { $eq: ["$airline.name", "$$airlines"] } }
 
             ], as: "parceiras"
         }
     },
     {
-        $addFields:
-            { count: { $size: "parceiras" } }
+        $unwid: "$parceiras"
     }, // descubra qual delas tem o maior número de rotas com esses aviões.
-    { $group: { _id: "$name", "totalRotas": { $sum: "$count" } } },
+    { $group: { _id: "$name", "totalRotas": { $sum: 1 } } },
     { $sort: { totalRotas: -1 } },
     { $limit: 1 } // mostra que tem mais rotas com esses aviões.
 ]);
