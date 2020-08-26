8b151d22afe8a42c50b20425deabbb63b1b3f517
wenderson <contato@wendersonbelko.com>
2020-08-26 18:59:10 -0300

    aaaaaa

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,25 +1,24 @@
 db.trips.aggregate([
     {
-        $addFields: { day: { $dayOfWeek: "$startTime" } },  // dias da semana / viagens
-    },
-    {
-        match: {
-            diaDaSemana: 5,
+        $addFields: {
+            sexta: { $dayOfWeek: "$startTime" } // dias da semana / viagens
         }
     },
+    { $match: { sexta: { $eq: 5 } } },
     {
         $group: { // soma das viagens do dia da semana
-            _id: "$day",
-            total: { $sum: 1 },
-        },
+            "_id": "$startStationName",
+            "total": { $sum: 1 }
+        }
     },
     {
         $project: { // Agora que você já sabe o dia com mais viagens, determine qual estação tem o maior número de viagens nesse dia da semana
             "_id": 0,
-            "nomeEstacao": "$_id.nomeEstacao",
-            "total": "$total"
+            "nomeEstacao": "$_id",
+            "total": "$total",
+
         }
     },
-    { $sort: { total: -1 } }, // ordena para pegar o dia com mais viagem
-    { $limit: 1 }, // pega o primeiro dia com mais viagem
+    { $sort: { "total": -1 } }, // ordena para pegar o dia com mais viagem
+    { $limit: 1 } // pega o primeiro dia com mais viagem
 ]);
