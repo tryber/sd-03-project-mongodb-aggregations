72c7606b6c45d754e35471f455665dadcb5f9b2f
tamagoshii <estelaap_martins@hotmail.com>
2020-08-27 22:37:17 -0300

    desafio 11 a 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,37 @@
+db.trips.aggregate([
+  {
+    $addFields: {
+      "count": {
+        // Divide o resultadoda subtração por 60 mil ms
+        $divide: [{
+          $abs: {
+            $subtract: ["$stopTime", "$startTime"]
+          }
+        }, 60000]
+      }
+    }
+  },
+  {
+    $group: {
+      "_id": "$bikeid",
+      "duracaoMedia": { $avg: "$count" }
+    }
+  },
+  {
+    $project: {
+      "_id": 0,
+      "bikeId": "$bikeid",
+      "duracaoMedia": {
+        $ceil: "$duracaoMedia"
+      }
+    }
+  },
+  {
+    $sort: {
+      "duracaoMedia": -1
+    }
+  },
+  {
+    $limit: 5
+  }
+]);
